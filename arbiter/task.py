65ba46a1f0a7bf13c506a33af39ce51baa5bc875
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-08-12 13:32:30 -0500

    create_task & retry

---
+++ arbiter/task.py
@@ -0,0 +1,94 @@
+"""
+Task Creation for arbiter
+"""
+from collections import namedtuple
+from datetime import timedelta
+from functools import wraps
+from numbers import Integral
+from time import sleep
+
+
+# Task is implemented as a namedtuple not a class. Task is used as a
+# description of how to accomplish a task, not the task itself (e.g.,
+# the task's status, etc. are not contained within the task), and there
+# aren't any methods associated with the task data. As such, it made
+# more sense to view a task as just data. namedtuple was chosen over
+# dict because the data is immutable, and attributes are completely
+# known in advance.
+
+# If you plan on creating a Task directly, instead of using create,
+# function should be a zero-argument function that returns True if
+# successful and False (or raises an exception) on failure. dependencies
+# should be a frozenset (a set would also work, but there is no need for
+# mutability).
+Task = namedtuple('Task', ['name', 'function', 'dependencies'])
+
+
+def create(name, function, dependencies=None):
+    """
+    Create a new task to be run by arbiter.
+
+    name: The name of the task being created. As the name is used for
+        dependency-tracking, it must be unique among all tasks you are
+        running at a given time.
+    function: A zero-argument function that returns True if successful
+        and False (or rasies an exception) on failure.
+    dependencies: (optional, None) An iterable of names of tasks that
+        must be successfully completed in order for this task to be run.
+    """
+    if dependencies is None:
+        dependencies = frozenset()
+    else:
+        dependencies = frozenset(dependencies)
+
+    return Task(name, function, dependencies)
+
+
+def retry(retries, delay=timedelta()):
+    """
+    A decorator for making a function that retries on failure (failure
+    is defined as returning a Falsy value, or raising an exception).
+
+    The function's returned value will be passed through (and if on the
+    final retry, an exception will be passed through).
+
+    retries: The number of times to retry if a failure occurs.
+    delay: (optional, 0 seconds) A timedelta representing the amount of
+        time to delay between retries.
+    """
+    if not isinstance(retries, Integral):
+        raise TypeError(retries)
+
+    delay_in_seconds = delay.total_seconds()
+
+    if delay_in_seconds < 0:
+        raise TypeError(delay)
+
+    def decorator(function):
+        """
+        The actual decorator for retrying.
+        """
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            """
+            The actual wrapper for retrying.
+            """
+            attempts = 0
+
+            while attempts < retries:
+                try:
+                    result = function(*args, **kwargs)
+
+                    if result:
+                        return result
+                except Exception:
+                    pass
+
+                attempts += 1
+                sleep(delay_in_seconds)
+
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
---
+++ tests/test_task.py
@@ -0,0 +1,182 @@
+"""
+Tests for the task submodule.
+"""
+from datetime import timedelta
+
+from nose.tools import assert_equal, assert_true, assert_false, assert_raises
+
+
+def test_create():
+    """
+    Test the create function.
+    """
+    from arbiter.task import create, Task
+
+    function = lambda x: True
+
+    # No dependencies
+    assert_equal(
+        create('foo', function),
+        Task('foo', function, frozenset())
+    )
+
+    # Dependencies
+    actual = create('foo', function, {'bar', 'baz'})
+    assert_equal(actual, Task('foo', function, frozenset(('bar', 'baz'))))
+    assert_equal(type(actual.dependencies), frozenset)
+
+    # passing an iterable
+    actual = create('foo', function, (name for name in ('bar', 'baz')))
+    assert_equal(actual, Task('foo', function, frozenset(('bar', 'baz'))))
+    assert_equal(type(actual.dependencies), frozenset)
+
+
+def test_retry():
+    """
+    Test the retry decorator.
+    """
+    from arbiter.task import retry
+
+    def retrier(retries, responses, delay=timedelta()):
+        """
+        A function for testing the retry decorator.
+        """
+        @retry(retries, delay)
+        def function():
+            """
+            Function that is wrapped by retry
+            """
+            response = responses.pop(0)
+
+            if isinstance(response, BaseException):
+                raise response
+
+            return response
+
+        return function()
+
+    # error on invalid retry count
+    with assert_raises(TypeError):
+        retrier(1.1, [True])
+
+    # no need to retry
+    assert_true(retrier(1, [True, False]))
+
+    # no retries allowed
+    assert_false(retrier(0, [False, True]))
+
+    # not enough retries
+    assert_false(retrier(2, [False, False, False, True]))
+
+    # success on retires
+    assert_true(retrier(2, [False, False, True]))
+
+    # don't raise if retries remaining
+    assert_false(retrier(2, [Exception(), TypeError(), False]))
+    assert_true(retrier(2, [Exception(), False, True]))
+
+    # raise if no retries remain
+    with assert_raises(ImportError):
+        retrier(2, [TypeError(), AttributeError(), ImportError()])
+
+    # make sure Falsy/Truthy, not False/True only
+    assert_equal(retrier(3, [[], timedelta(), None, [1, 2, 3]]), [1, 2, 3])
+    assert_equal(retrier(2, [[], 1]), 1)
+
+    # Don't let retry catch kill exceptions
+    with assert_raises(KeyboardInterrupt):
+        retrier(1, [KeyboardInterrupt(), True])
+
+    with assert_raises(SystemExit):
+        retrier(1, [SystemExit(), True])
+
+
+def test_retry_delay():
+    """
+    Tests for the delay functionality in the retry wrapper.
+    """
+    from arbiter.task import retry
+
+    from arbiter import task
+
+    call_stack = []
+
+    def mock_sleep(seconds):
+        """
+        Mock version of sleep to ensure retry is sleeping the righ
+        lengths, and the right period of time.
+        """
+        call_stack.append(('sleep', seconds))
+
+    task.sleep = mock_sleep
+
+    def retrier(retries, responses, delay=None):
+        """
+        A function for testing the retry decorator.
+        """
+        if delay is None:
+            kwargs = {}
+        else:
+            kwargs = {'delay': delay}
+
+        @retry(retries, **kwargs)
+        def function():
+            """
+            Function that is wrapped by retry
+            """
+            response = responses.pop(0)
+
+            call_stack.append(('call', response))
+
+            if isinstance(response, Exception):
+                raise response
+
+            return response
+
+        return function()
+
+    # default settings
+    assert_true(retrier(0, [True]))
+    assert_equal(call_stack, [('call', True)])
+
+    call_stack = []
+    assert_true(retrier(1, [True]))
+    assert_equal(call_stack, [('call', True)])
+
+    call_stack = []
+    assert_true(retrier(1, [False, True]))
+    assert_equal(call_stack, [('call', False), ('sleep', 0), ('call', True)])
+
+    call_stack = []
+    assert_false(retrier(1, [False, False]))
+    assert_equal(call_stack, [('call', False), ('sleep', 0), ('call', False)])
+
+    # delays
+    call_stack = []
+    assert_false(retrier(1, [False, False], delay=timedelta(seconds=55)))
+    assert_equal(call_stack, [('call', False), ('sleep', 55), ('call', False)])
+
+    # invalid delays
+    with assert_raises(AttributeError):
+        retrier(1, [False, False], delay=5)
+
+    with assert_raises(TypeError):
+        retrier(1, [False, False], delay=timedelta(seconds=-1))
+
+    # duck typing?
+    class Delay(object):
+        """
+        Test that retry is duck typing on delay
+        """
+        def __init__(self, delay):
+            self.delay = delay
+
+        def total_seconds(self):
+            """
+            All that's really required
+            """
+            return self.delay
+
+    call_stack = []
+    assert_false(retrier(1, [False, False], delay=Delay(10)))
+    assert_equal(call_stack, [('call', False), ('sleep', 10), ('call', False)])
