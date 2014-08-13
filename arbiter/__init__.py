fa9c6015dbc13066e302fefd79c3fcff691b8f3c
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-08-13 11:22:05 -0500

    make create_tasks available from the root module

--- arbiter/__init__.py
+++ arbiter/__init__.py
@@ -1,3 +1,4 @@
 """
 A concurrent task-runner that automatically resolves dependency issues.
 """
+from arbiter.task import create_task
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,15 @@
 """
 Tests for the arbiter module
 """
+from nose.tools import assert_equal
+
+
+def test_paths():
+    """
+    Test that functions are avaialable from the base module.
+    """
+    import arbiter
+
+
+    from arbiter.task import create_task
+    assert_equal(arbiter.create_task, create_task)
